e68cb20bcf18eaf57e36d5385772a3f64ea8b54e
Prashantoli <137768803+Prashantoli@users.noreply.github.com>
2024-08-23 16:27:31 +0545

    chapter3

---
+++ stringfunction.py
@@ -0,0 +1,15 @@
+# some string functions are discussed below
+# len() function
+name = "prashant"
+print(len(name))# it return the length of the string
+a=name.endswith("ant") # it tells whether the variable string end with ant or not
+print(a)
+
+b=name.count("a") # it count the number of string 
+print(b)
+
+c=name.startswith("pr") 
+print(c)
+
+d=name.capitalize()
+print(d)
---
+++ stringrule.py
@@ -0,0 +1,11 @@
+name = "prashant"
+sl = name[0:6]
+print(sl)
+
+hello = "sahara"
+pi = hello[-5:-1]
+print(pi)
+
+hey ="prashantoli"
+i=hey[0:7:2]
+print(i)
